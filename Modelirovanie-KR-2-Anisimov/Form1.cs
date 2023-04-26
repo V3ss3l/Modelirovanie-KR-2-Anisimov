d9238696a4f268c65e823d9ef16a41ca325c0b02
V3ss3l <grishav3ss3l@gmail.com>
2023-04-26 17:15:42 +0400

    Добавьте файлы проекта.

---
+++ Modelirovanie-KR-2-Anisimov/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modelirovanie_KR_2_Anisimov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            OperationalDevice
+            
+        }
+    }
+}
---
+++ Modelirovanie-KR-2-Anisimov/OperationalDevice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelirovanie_KR_2_Anisimov
+{
+    class OperationalDevice
+    {
+        private bool[] _states; // A1, A2, A3 ...
+        private int _currentState;
+        private int _prevState;
+        private bool[] _conditions; // X1, X2, X3, ...
+        private bool[] _operations; // Y1, Y2, Y3, ...
+        private bool _PP;
+
+        public OperationalDevice() {
+            _states = new bool[11];
+            _currentState = 0;
+            _prevState = _currentState;
+            _conditions = new bool[7];
+            _operations = new bool[12];
+        }
+
+       public void StateMemory(byte stateCode)
+        {
+
+            _currentState = stateCode; 
+            //int stateIndex = BinToDec(stateCode.ToString());
+            _states[_prevState] = false;
+            _states[_currentState] = true;
+            _prevState = _currentState;
+        }
+
+        public void ConditionMemory(ushort conditionCode) { 
+
+        }
+
+        /*private int BinToDec(string bin)
+        {
+            int dec = 0;
+            for(int i = 0, j = bin.Length-1; i < bin.Length; i++, j--) {
+                dec += bin[i] == '1' ? (int)Math.Pow(2, j) : 0;
+            }
+            return dec;
+        }*/
+    }
+}
